26fa97c0f0ffc4edd78cb82e2a65482e4248a399
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-03-25 01:19:39 +0100

    Add search

--- src/pages/PasswordsPage/index.tsx
+++ src/pages/PasswordsPage/index.tsx
@@ -1,5 +1,6 @@
 import "./index.scss";
 import {
+  IonButton,
   IonContent,
   IonItem,
   IonLabel,
@@ -9,9 +10,27 @@ import {
   IonThumbnail,
 } from "@ionic/react";
 import { useGlobalContext } from "../../hooks/useGlobalContext";
+import React, { useEffect, useState } from "react";
+import { AccountItem } from "../../hooks/useStorage";
 
-export const PasswordsPage = () => {
+export const PasswordsPage: React.FC = () => {
   const { AccountsState } = useGlobalContext();
+  const [nameFilter, setNameFilter] = useState("");
+  const [searchAccount, setSearchAccount] = useState<AccountItem[]>([]);
+
+  useEffect(() => {
+    if (nameFilter.length > 0) {
+      const filterAccounts = AccountsState.accounts.filter((account) => {
+        if (
+          account.website.substring(0, nameFilter.length).toLowerCase() ===
+          nameFilter.toLowerCase()
+        ) {
+          return account;
+        }
+      });
+      setSearchAccount(filterAccounts);
+    }
+  }, [nameFilter]);
 
   return (
     <IonPage>
@@ -20,27 +39,72 @@ export const PasswordsPage = () => {
           <h1>My Passwords</h1>
         </header>
         <main>
-          <IonSearchbar className="search-bar"></IonSearchbar>
-          <IonList>
-            {AccountsState.accounts.map((account) => (
-              <IonItem
-                key={account.id}
-                routerLink={`/app/password/${account.id}`}
-              >
-                <IonThumbnail slot="start">
-                  <img
-                    alt={account.website}
-                    src={`https://besticon-demo.herokuapp.com/icon?url=${account.linkWebsite}&size=80..120..200`}
-                  />
-                </IonThumbnail>
-                <IonLabel>
-                  <h2>{account.website}</h2>
-                  <h6>{account.email}</h6>
-                </IonLabel>
-                {/* <IonIcon icon={chevronForwardOutline} slot="end" /> */}
-              </IonItem>
-            ))}
-          </IonList>
+          {AccountsState.accounts.length === 0 && (
+            <div className="empty-password">
+              <h1>Nessuna Password!</h1>
+              <IonButton routerLink="/app/add-account">Aggiungi</IonButton>
+            </div>
+          )}
+          {AccountsState.accounts.length > 0 && (
+            <>
+              <IonSearchbar
+                className="search-bar"
+                value={nameFilter}
+                onIonChange={(e) => setNameFilter(e.detail.value!)}
+              ></IonSearchbar>
+              <IonList>
+                {/* Se c'e un filtro mosto array filtrato */}
+                {nameFilter.length > 0 &&
+                  searchAccount.length > 0 &&
+                  searchAccount.map((account, index) => (
+                    <IonItem
+                      key={account.id}
+                      routerLink={`/app/password/${account.id}`}
+                    >
+                      <IonThumbnail slot="start">
+                        <img
+                          alt={account.website}
+                          src={`https://besticon-demo.herokuapp.com/icon?url=${account.linkWebsite}&size=80..120..200`}
+                        />
+                      </IonThumbnail>
+                      <IonLabel>
+                        <h2>{account.website}</h2>
+                        <h6>{account.email}</h6>
+                      </IonLabel>
+                      {/* <IonIcon icon={chevronForwardOutline} slot="end" /> */}
+                    </IonItem>
+                  ))}
+
+                {/* Se non ho filtri mostro tutti gli account */}
+                {nameFilter.length === 0 &&
+                  AccountsState.accounts.map((account, index) => (
+                    <IonItem
+                      key={account.id}
+                      routerLink={`/app/password/${account.id}`}
+                    >
+                      <IonThumbnail slot="start">
+                        <img
+                          alt={account.website}
+                          src={`https://besticon-demo.herokuapp.com/icon?url=${account.linkWebsite}&size=80..120..200`}
+                        />
+                      </IonThumbnail>
+                      <IonLabel>
+                        <h2>{account.website}</h2>
+                        <h6>{account.email}</h6>
+                      </IonLabel>
+                      {/* <IonIcon icon={chevronForwardOutline} slot="end" /> */}
+                    </IonItem>
+                  ))}
+
+                {/* Se ho il filtro ma l'array filtrato è vuoto */}
+                {nameFilter.length > 0 && searchAccount.length === 0 && (
+                  <div className="not-found">
+                    <h1>Nessun risultato trovato!</h1>
+                  </div>
+                )}
+              </IonList>
+            </>
+          )}
         </main>
       </IonContent>
     </IonPage>
