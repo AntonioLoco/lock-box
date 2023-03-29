fc94624da823d0a40713974c6c2daa49a5b78143
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-03-29 11:36:55 +0200

    Add category

--- src/categories.ts
+++ src/categories.ts
@@ -12,9 +12,141 @@ export const categories = [
             { name: 'TikTok', icon: 'tiktok.svg' },
             { name: 'Reddit', icon: 'reddit.svg' },
             { name: 'Discord', icon: 'discord.svg' },
-            { name: 'Twitch', icon: 'twitch.svg' },
-            { name: 'Skype', icon: 'skype.svg' },
             { name: 'Tumblr', icon: 'tumblr.svg' },
+            { name: 'BeReal', icon: 'bereal.svg' },
+        ]
+    },
+    {
+        category: "Intrattenimento",
+        apps: [
+            { name: 'Netflix', icon: 'netflix.svg' },
+            { name: 'Dazn', icon: 'dazn.svg' },
+            { name: 'Twitch', icon: 'twitch.svg' },
+            { name: 'Spotify', icon: 'spotify.svg' },
+            { name: 'Disnay+', icon: 'disney.svg' },
+            { name: 'Itunes', icon: 'itunes.svg' },
+            { name: 'Prime Video', icon: 'prime_video.svg' },
+            { name: 'Amazon Music', icon: 'amazon_music.svg' },
+            { name: 'Tidal', icon: 'tidal.svg' },
+            { name: 'Rai Play', icon: 'rai_play.svg' },
+            { name: 'Pluto TV', icon: 'pluto_tv.svg' },
+            { name: 'Mediset Infinity', icon: 'mediaset_infinity.svg' },
+            { name: 'Sky', icon: 'sky.svg' },
+            { name: 'Viki Rakuten', icon: 'viki.svg' },
+            { name: 'Paramount+', icon: 'paramount.svg' },
+            { name: 'Now TV', icon: 'now.svg' },
+        ]
+    },
+    {
+        category: "Shopping",
+        apps: [//Icone da fare
+            {name: "Shein", icon: "shein.svg"},
+            {name: "Vinted", icon: "vinted.svg"},
+            {name: "Amazon", icon: "amazon.svg"},
+            {name: "Subito", icon: "subito.svg"},
+            {name: "Zalando", icon: "zalando.svg"},
+            {name: "About You", icon: "about_you.svg"},
+            {name: "Sephora", icon: "sephora.svg"},
+            {name: "Wallapop", icon: "wallapop.svg"},
+            {name: "Notino", icon: "notino.svg"},
+            {name: "Asos", icon: "asos.svg"},
+            {name: "Mediaworld", icon: "mediaworld.svg"},
+            {name: "Euronics", icon: "euronics.svg"},
+            {name: "Unieuro", icon: "unieuro.svg"},
+        ]
+    },
+    {
+        category: "Produttività",
+        apps: [ 
+            {name: "Gmail", icon: "gmail.svg"},
+            {name: "Microsoft", icon: "microsoft.svg"},
+            {name: "Apple", icon: "apple.svg"},
+            {name: "Libero", icon: "libero.svg"},
+            {name: "Xiaomi", icon: "xiaomi.svg"},
+            {name: "Zoom", icon: "zoom.svg"},
+            {name: "Trello", icon: "trello.svg"},
+            {name: "Slack", icon: "slack.svg"},
+            { name: 'Skype', icon: 'skype.svg' },
+            {name: "Canva", icon: "canva.svg"},
+            {name: "GitHub", icon: "gihub.svg"},
+            {name: "PicsArt", icon: "picsart.svg"},
+            {name: "Adobe", icon: "adobe.svg"},
+        ]
+    },
+    {
+        category: "Istruzione",
+        apps: [
+            {name: "didUp", icon: "didup.svg"},
+            {name: "18app", icon: "18app.svg"},
+            {name: "ClasseViva", icon: "classeviva.svg"},
+            {name: "Udemy", icon: "udemy.svg"},
+            {name: "MIUR", icon: "miur.svg"},
+        ]
+    },
+    {
+        category: "Utility",
+        apps: [
+            {name: "Poste", icon: "poste.svg"},
+            {name: "INPS", icon: "inps.svg"},
+            {name: "Enel energia", icon: "enel.svg"},
+            {name: "Vodafone", icon: "vodafone.svg"},
+            {name: "Fastweb", icon: "fastweb.svg"},
+            {name: "Ho", icon: "ho.svg"},
+            {name: "Nord VPN", icon: "nordvpn.svg"},
+            {name: "Very Mobile", icon: "very_mobile.svg"},
+            {name: "Ookla Speedtest", icon: "ookla.svg"},
+            {name: "Tim", icon: "tim.svg"},
+            {name: "Kena Mobile", icon: "kena.svg"},
+        ]
+    },
+    {
+        category: "Finanza",
+        apps: [
+            {name: "PostePay", icon: "postepay.svg"},
+            {name: "Banco Posta", icon: "bancoposta.svg"},
+            {name: "PayPal", icon: "paypal.svg"},
+            {name: "Intesa San Paolo", icon: "intesasanpaolo.svg"},
+            {name: "Unicredit", icon: "unicredit.svg"},
+            {name: "Satispay", icon: "satispay.svg"},
+            {name: "Revolout", icon: "revoulut.svg"},
+            {name: "Generali", icon: "generali.svg"},
+            {name: "BuddyBank", icon: "buddybank.svg"},
+            {name: "Banca MPS", icon: "bancamps.svg"},
+            {name: "ING", icon: "ing.svg"},
+            {name: "Crypto", icon: "crypto.svg"},
+            {name: "BLN", icon: "bln.svg"},
+            {name: "YAP", icon: "yap.svg"},
+            {name: "CoinBase", icon: "coinbase.svg"},
+            {name: "Sella", icon: "sella.svg"},
+            {name: "Allianz", icon: "allianz.svg"},
+            {name: "Skrill", icon: "skrill.svg"},
+            {name: "HYPE", icon: "hype.svg"},
+            {name: "Compass", icon: "compass.svg"},
+        ]
+    },
+    {
+        category: "Viaggi",
+        apps: [
+            {name: "Booking", icon: "booking.svg"},
+            {name: "Ryanair", icon: "ryanair.svg"},
+            {name: "AirBnb", icon: "airbnb.svg"},
+            {name: "Trenitalia", icon: "trenitalia.svg"},
+            {name: "Uber", icon: "uber.svg"},
+            {name: "eDreams", icon: "edreams.svg"},
+            {name: "ITA Airways", icon: "itaairways.svg"},
+            {name: "Flix Bus", icon: "flixbus.svg"},
+            {name: "TripAdvisor", icon: "tripadvisor.svg"},
+        ]
+    },
+    {
+        category: "Cibi",
+        apps: [
+            {name: "Just Eat", icon: "justeat.svg"},
+            {name: "McDonald's", icon: "mcdonalds.svg"},
+            {name: "Deliveroo", icon: "deliveroo.svg"},
+            {name: "Burger King", icon: "burgerking.svg"},
+            {name: "KFC", icon: "kfc.svg"},
+            {name: "Glovo", icon: "glovo.svg"},
         ]
     }
 ]
